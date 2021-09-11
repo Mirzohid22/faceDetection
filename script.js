b5066d57fe85ef39902429eecb99ac8a42bef166
Mirzohid22 <smm2226fstd@gmail.com>
2021-09-11 16:05:13 +0500

    models path changed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video');
 async function parallel() {
  await Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('https://github.com/Mirzohid22/faceDetection/tree/master/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('https://github.com/Mirzohid22/faceDetection/tree/master/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('https://github.com/Mirzohid22/faceDetection/tree/master/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('https://github.com/Mirzohid22/faceDetection/tree/master/models')
   ]).then(startVideo).catch(e => console.error(e));
 }
 parallel();
