60cb7a489963198ff3304ea3a1d1f567e91d97d6
Mirzohid22 <smm2226fstd@gmail.com>
2021-09-11 14:40:53 +0500

    changes on promises face-api.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-const video = document.getElementById('video')
-
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+const video = document.getElementById('video');
+async function parallel() {
+ await Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo);
+}
+parallel();
 
 function startVideo() {
   navigator.getUserMedia(
